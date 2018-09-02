3a9637342cefb900db7c5447aa9056767d4d08f8
hanntronico <pedro.bernal.84@gmail.com>
2018-09-02 12:22:05 -0500

    armando el archivo de reportes.php

--- reportes.php
+++ reportes.php
@@ -1,5 +1,42 @@
 <?php
-
-	echo "aquí los reportes";
-
+	// echo "aquí los reportes";
 ?>
+
+<script src="assets/js/hann.js" type="text/javascript" charset="utf-8"></script>
+
+<div class="outer">
+	<div class="inner bg-light lter">
+		<div class="row">
+
+			<div class="col-lg-12">
+				<div class="block">
+					<span id="respuesta" class="label label-success"></span>
+				</div>
+			</div>
+
+			<div class="col-lg-12">
+				<div class="box">
+					<header>
+						<div class="icons">
+							<i class="fa fa-list-alt"></i>
+						</div>
+						<h5>Reportes</h5>
+					</header><!-- /header -->					
+				</div>
+			</div>
+			
+		</div>
+
+		<div class="row">
+			<div class="col-lg-12">
+				<div class="box" style="padding: 10px;">
+					hanntronico
+					
+				</div>
+			</div>
+		</div>
+
+		<div style="height: 300px;"></div>  
+		<div style="height: 300px;"></div>  
+	</div>
+</div>
