413005efaa4b3aaae83585cbaafe672fb19d09ff
hanntronico <pedro.bernal.84@gmail.com>
2018-09-10 10:53:25 -0500

    cambio en el conectar_server.php

--- conectar_server.php
+++ conectar_server.php
@@ -2,7 +2,7 @@
 
 function Conectarse()
 {
-	if (!($enlace=mysql_connect("127.0.0.1","may10ind_userbd","*274053@2018*")))
+	if (!($enlace=mysql_connect("localhost","may10ind_userbd","*274053@2018*")))
 	{
 	echo "ERROR EN LA CONEXION: NUEVOS ELEMENTOS HAN SIDO CREADOS";
 	exit();
